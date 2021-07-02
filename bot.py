3347b6129e0827e064ff95a68843f29739cec701
42bahn <bbu0704@gmail.com>
2021-07-03 01:25:32 +0900

    commit

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@ import discord, asyncio
 from discord.ext import commands
 import os
 
-import member
-
 bot = commands.Bot(command_prefix='.', help_command=None)
 
 commands = ['.help', 'ㅎㅇㅇ', 'ㅎㅇ']
@@ -32,22 +30,21 @@ async def hello2(ctx):
 
 @bot.command()
 async def 송인철(ctx):
-    member.송인철(ctx)
-    # embed = discord.Embed(title = "송 인철",
-    # description = "", color = 0x62c1cc)
-    # embed.add_field(name = "생년월일", value = "1897년 10월 24일", inline = False)
-    # embed.add_field(name = "직업", value = "Secret Agent", inline = False)
-    # embed.add_field(name = "취미", value = "애니메이션 감상", inline = False)
-    # await ctx.send(embed = embed)
+    embed = discord.Embed(title = "송 인철",
+    description = "", color = 0x62c1cc)
+    embed.add_field(name = "생년월일", value = "1897년 10월 24일", inline = False)
+    embed.add_field(name = "직업", value = "Secret Agent", inline = False)
+    embed.add_field(name = "취미", value = "애니메이션 감상", inline = False)
+    await ctx.send(embed = embed)
 
-# @bot.command()
-# async def 손나성(ctx):
-#     embed = discord.Embed(title = "손 나성",
-#     description = "", color = 0x62c1cc)
-#     embed.add_field(name = "생년월일", value = "1997년 식목일", inline = False)
-#     embed.add_field(name = "직업", value = "알수없음", inline = False)
-#     embed.add_field(name = "취미", value = "알코올 섭취", inline = False)
-#     await ctx.send(embed = embed)
+@bot.command()
+async def 손나성(ctx):
+    embed = discord.Embed(title = "손 나성",
+    description = "", color = 0x62c1cc)
+    embed.add_field(name = "생년월일", value = "1997년 식목일", inline = False)
+    embed.add_field(name = "직업", value = "알수없음", inline = False)
+    embed.add_field(name = "취미", value = "알코올 섭취", inline = False)
+    await ctx.send(embed = embed)
 
 @bot.command(name='help')
 async def embed(ctx) :
