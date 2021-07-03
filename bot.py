aa24cb38e0771907225978843ee35f4b8e128111
42bahn <bbu0704@gmail.com>
2021-07-03 16:11:04 +0900

    commit

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import random
 from discord.flags import alias_flag_value
 
 from module.dice import dice
-from module.member import 송인철
+from module.member import 송인철, 손나성, 임석민, 김태훈, 김요환
 
 bot = commands.Bot(command_prefix='.', help_command=None)
 
@@ -66,48 +66,22 @@ async def hello3(ctx):
 @bot.command(aliases=['송인철', '감자통모짜', '20시 유산균을 먹자'])
 async def call_송인철(ctx):
     await 송인철(ctx)
-    # embed = discord.Embed(title = "송 인철",
-    # description = "", color = 0x62c1cc)
-    # embed.add_field(name = "생년월일", value = "1897년 10월 24일", inline = False)
-    # embed.add_field(name = "직업", value = "Secret Agent", inline = False)
-    # embed.add_field(name = "취미", value = "애니메이션 감상", inline = False)
-    # await ctx.send(embed = embed)
 
-@bot.command(aliases=['후', '후...'])
-async def 손나성(ctx):
-    embed = discord.Embed(title = "손 나성",
-    description = "", color = 0x62c1cc)
-    embed.add_field(name = "생년월일", value = "1997년 식목일", inline = False)
-    embed.add_field(name = "직업", value = "알수없음", inline = False)
-    embed.add_field(name = "취미", value = "알코올 섭취", inline = False)
-    await ctx.send(embed = embed)
+@bot.command(aliases=['손나성', '후', '후...'])
+async def call_손나성(ctx):
+    await 손나성(ctx)
 
-@bot.command(aliases=['XTEN'])
-async def 임석민(ctx):
-    embed = discord.Embed(title = "임 석민",
-    description = "", color = 0x62c1cc)
-    embed.add_field(name = "생년월일", value = "1997년 1월 22일", inline = False)
-    embed.add_field(name = "직업", value = "개강을 간절히 기다리는 대학생", inline = False)
-    embed.add_field(name = "취미", value = "조별 과제", inline = False)
-    await ctx.send(embed = embed)
+@bot.command(aliases=['임석민', 'XTEN'])
+async def call_임석민(ctx):
+    await 임석민(ctx)
 
-@bot.command(aliases=['EggZegg', 'Eggzegg', 'eggZegg', 'eggzegg'])
-async def 김태훈(ctx):
-    embed = discord.Embed(title = "김 태훈",
-    description = "", color = 0x62c1cc)
-    embed.add_field(name = "생년월일", value = "1997년 12월 21일", inline = False)
-    embed.add_field(name = "직업", value = "CU 지박령", inline = False)
-    embed.add_field(name = "취미", value = "폐기 수집", inline = False)
-    await ctx.send(embed = embed)
+@bot.command(aliases=['김태훈', 'EggZegg', 'Eggzegg', 'eggZegg', 'eggzegg'])
+async def call_김태훈(ctx):
+    await 김태훈(ctx)
 
-@bot.command(aliases=['요비'])
-async def 김요환(ctx):
-    embed = discord.Embed(title = "김 요환",
-    description = "", color = 0x62c1cc)
-    embed.add_field(name = "생년월일", value = "1997년 5월 13일", inline = False)
-    embed.add_field(name = "직업", value = "수상한 공부방 긴 생머리 아저씨", inline = False)
-    embed.add_field(name = "취미", value = "머리카락 흩날리며 뒤돌아보기", inline = False)
-    await ctx.send(embed = embed)
+@bot.command(aliases=['김요환', '요비'])
+async def call_김요환(ctx):
+    await 김요환(ctx)
 ################## Member INFO END
 
 
