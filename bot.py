c69f304d1fa1d6a7c3298dbeae23bf9659b943ab
42bahn <bbu0704@gmail.com>
2021-07-08 20:18:42 +0900

    mic_check

--- bot.py
+++ bot.py
@@ -107,7 +107,7 @@ async def me(ctx):
 @bot.command(aliases=['ㅁㅊ', '맠쳌', '마이크체크', '마쳌', 'ㅁㅊㅁㅊ'])
 async def mic_check(ctx, *target):
     if len(target) == 0:
-        await ctx.send("요청할 유저들의 멘션(@username)을 기입해주세요.")
+        await ctx.send("Hi! @everyone. 모두 음성채널로 들어와주시기 바랍니다.")
     else:
         # await ctx.send(f"{target}씨! 음성채널로 들어오십시오.")
         await ctx.send('{}씨! 음성채널로 들어오십시오.'.format(', '.join(target)))
