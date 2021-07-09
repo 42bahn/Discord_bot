83a0f45561dbc828a4a160e544f14e1a3912f889
42bahn <bbu0704@gmail.com>
2021-07-09 13:34:52 +0900

    reply

--- module/member.py
+++ module/member.py
@@ -6,7 +6,7 @@ async def 송인철(ctx):
     embed.add_field(name = "생년월일", value = "1897년 10월 24일", inline = False)
     embed.add_field(name = "직업", value = "Secret Agent", inline = False)
     embed.add_field(name = "취미", value = "애니메이션 감상", inline = False)
-    await ctx.send(embed = embed)
+    await ctx.reply(embed = embed)
 
 async def 손나성(ctx):
     embed = discord.Embed(title = "손 나성",
@@ -14,7 +14,7 @@ async def 손나성(ctx):
     embed.add_field(name = "생년월일", value = "1997년 식목일", inline = False)
     embed.add_field(name = "직업", value = "알수없음", inline = False)
     embed.add_field(name = "취미", value = "알코올 섭취", inline = False)
-    await ctx.send(embed = embed)
+    await ctx.reply(embed = embed)
 
 async def 임석민(ctx):
     embed = discord.Embed(title = "임 석민",
@@ -22,7 +22,7 @@ async def 임석민(ctx):
     embed.add_field(name = "생년월일", value = "1997년 1월 22일", inline = False)
     embed.add_field(name = "직업", value = "개강을 간절히 기다리는 대학생", inline = False)
     embed.add_field(name = "취미", value = "조별 과제", inline = False)
-    await ctx.send(embed = embed)
+    await ctx.reply(embed = embed)
 
 async def 김태훈(ctx):
     embed = discord.Embed(title = "김 태훈",
@@ -30,7 +30,7 @@ async def 김태훈(ctx):
     embed.add_field(name = "생년월일", value = "1997년 12월 21일", inline = False)
     embed.add_field(name = "직업", value = "CU 지박령", inline = False)
     embed.add_field(name = "취미", value = "폐기 수집", inline = False)
-    await ctx.send(embed = embed)
+    await ctx.reply(embed = embed)
 
 async def 김요환(ctx):
     embed = discord.Embed(title = "김 요환",
@@ -38,7 +38,7 @@ async def 김요환(ctx):
     embed.add_field(name = "생년월일", value = "1997년 5월 13일", inline = False)
     embed.add_field(name = "직업", value = "수상한 긴 생머리 공부방 아저씨", inline = False)
     embed.add_field(name = "취미", value = "머리카락 흩날리며 뒤돌아보기", inline = False)
-    await ctx.send(embed = embed)
+    await ctx.reply(embed = embed)
 
 async def 안범준(ctx):
-    await ctx.send('금지어입니다.')
+    await ctx.reply('금지어입니다.')
