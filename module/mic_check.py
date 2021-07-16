d9f6bb5c37e95a9aef87209b02dc46bede23f476
42bahn <bbu0704@gmail.com>
2021-07-16 14:33:44 +0900

    mic_check_EXCEPTION_ISSUE

--- module/mic_check.py
+++ module/mic_check.py
@@ -3,4 +3,4 @@ async def ft_mic_check(ctx, *target):
         await ctx.reply("모두 음성채널로 들어와주시기 바랍니다. @everyone")
     else:
         # await ctx.send(f"{target}씨! 음성채널로 들어오십시오.")
-        await ctx.reply('{target}{}씨!\n 음성채널로 들어오십시오.'.format(', '.join(target)))
+        await ctx.reply('{}씨!\n 음성채널로 들어오십시오.'.format(', '.join(target)))
