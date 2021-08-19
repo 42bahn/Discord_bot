a47d66690185883535d15b777fc3f8a9d7a6e915
42bahn <bbu0704@gmail.com>
2021-08-19 14:15:31 +0900

    add_hakimi

--- module/member.py
+++ module/member.py
@@ -40,5 +40,15 @@ async def 김요환(ctx):
     embed.add_field(name = "취미", value = "머리카락 흩날리며 뒤돌아보기", inline = False)
     await ctx.reply(embed = embed)
 
+async def 김규철(ctx):
+    embed = discord.Embed(title = "아치라프 하키미 (Achraf Hakimi)",
+    description = "", color = 0x62c1cc)
+    embed.set_image("https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA2MjFfMTEz%2FMDAxNjI0Mjc3MzE4ODY1.p_dbUZNGyHQqYJxT8zDaZnHNLU00ahCev2OHmi-FfvAg.qF_ILAvEquYFThrv1s5SUjEG0q9hmrBqudJN48yo1Tgg.JPEG.tts0732%2FKakaoTalk_20210621_200750983_06.jpg&type=sc960_832")
+    embed.add_field(name = "생년월일", value = "1998년 11월 4일, 스페인", inline = False)
+    embed.add_field(name = "직업", value = "축구선수", inline = False)
+    embed.add_field(name = "신체", value = "181cm, 73kg", inline = False)
+    embed.add_field(name = "소속팀", value = "파리 생제르맹 FC (MF 미드필더, 2)", inline = False)
+    await ctx.reply(embed = embed)
+
 async def 안범준(ctx):
     await ctx.reply('금지어입니다.')
